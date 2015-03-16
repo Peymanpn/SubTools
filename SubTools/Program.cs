5c18655524a828cc8263a9e6cb952749242e297e
Peyman <peymanpn@yahoo.com>
2015-03-17 01:22:07 +0330

    frmMain in whole project changed

--- SubTools/Program.cs
+++ SubTools/Program.cs
@@ -14,7 +14,7 @@ namespace SubTools
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
