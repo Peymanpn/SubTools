70fa5a6974383d4be240916fd072352e4217c084
Peyman <peymanpn@yahoo.com>
2015-03-20 05:35:11 +0330

    ml commit 2

---
+++ SubTools/IPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDStudio.SubTools
+{
+    public delegate void StartPlugin();
+    interface IPlugin
+    {
+        string ClassName { get; }
+        System.Version Version { get; }
+        StartPlugin StartingMethod { get; }
+        System.Windows.Forms.Form MainForm { get; set; }
+    }
+
+    public class Plugin
+    {
+        public static bool PluginsDetected
+        {
+            get;
+            set;
+        }
+
+
+    }
+
+}
